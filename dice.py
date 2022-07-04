ba6bd961c4efdf0d7cda9a710133e5f6403cddf9
Szhmidty <44562499+Szhmidty@users.noreply.github.com>
2022-07-04 18:30:21 -0400

    Add files via upload

--- dice.py
+++ dice.py
@@ -1,105 +1,214 @@
-from functools import lru_cache
+from functools import lru_cache, reduce
+import operator as op
 import timeit
 
-@lru_cache(50000)
-def simplical_number(n,x):
-    if n < 0 and x < 0:
+@lru_cache(1000000)
+def simplexVolume(dim: int, n: int):
+    """
+    simplexVolume(dim: int, n: int)
+
+    Calculates the the nth simplical number of dimension dim 
+
+    By analogy, this function returns the volume of a simplex of dimension dim
+    made up of unit cubes
+    """
+    if dim < 1 or n < 1:
         return 0
     p = 1
-    for it in range(n):
-        p *= (x + it)/(it + 1)
+    for it in range(dim):
+        p *= (n + it)/(it + 1)
     return p
 
-@lru_cache(50000)
-def pascal(row:int, col:int) -> int:
-    if row<0:
-        return 0
-    if col>row:
-        return 0
-    if row==0 and col==0:
-        return 1
+@lru_cache(1000000)
+def truncatedSimplexVolume(constraints, n, dim = None):
+    """
+    truncatedSimplexVolume(constraints, n, dim = None)
 
-    return pascal(row - 1, col) + pascal(row - 1, col - 1)
+    Calculates the volume of a truncated simplex of size n and dimension dim by
+    truncating the simplex at the boundaries defined by the tuple constraints
 
-def dice_roll_probability_at_most_pascal(n: int, N: int, roll:int) -> float:
-    roll = roll - n
-    pos = roll // N # which stage of adding/substracting simplex volumes we've reached
-                    # strictly speaking not necessary, we can simply define pascal/simplical_number
-                    # as being 0 for negative/out of scope inputs, but this saves us from having to
-                    # even call the function for those inputs/save them to the cache
+    if dim is not set, it is assumed to be the length of the constraints iterable
+
+    e.g. truncatedSimplexVolume([3,4,5], 10) will calculate a 3d simplex of size 10
+    (i.e. it will call simplexVolume(3,10)) and then eliminate all volume that extends
+    beyond X<=3, Y<=4, Z<=5 i.e. it will be truncated to fit within a rectangular prism
+    of length 3, width 4, and height 5. 
+
+    Note that constraints should be a tuple, this function is cached and all inputs
+    must be hashable
+    """
+    if dim is None:
+        dim = len(constraints)
+
+    # start with the full volume for a simplex of dimension dim and size n
+    volume = simplexVolume(dim, n)
+    
+    # for each constraint, subtract off the protrusion along a given dimension
+    # eliminating a constraint at each step
+    for it in range(len(constraints)):
+        volume -= truncatedSimplexVolume(constraints[it+1:], n-constraints[it], dim)
+    return volume
+
+def symmetricalTruncatedSimplexVolume(numberOfConstraints, constraint, n, dim = None):
+    """
+    symmetricalTruncatedSimplexVolume(numberOfConstraints, constraint, n, dim = None)
+
+    Calculates the volume of a truncated simplex of size n and dimension dim by
+    truncating the simplex at the boundaries defined by the number of constraints
+    and the constraint value
+
+    This function behaves similarly to truncatedSimplexVolume, however this function
+    assumes the constraint of each dimension is the same, and so instead takes the number
+    of constrained dimensions and the size of the constraint
+
+    This particular case can be heavily optimized, and so runs much quicker than the general
+    case
+
+    if dim is not set, it is assumed to be the same as numberOfConstraints
+
+    e.g. symmetricalTruncatedSimplexVolume(3,5,10) will calculate a 3d simplex of size 10
+    (i.e. it will call simplexVolume(3,10)) and then eliminate all volume that extends
+    beyond X<=5, Y<=5, Z<=5 i.e. it will be truncated to fit within a cube of size 5
+    """
+    
+    if dim is None:
+        dim = numberOfConstraints
+    pos = n // constraint
     s = 0
     for it in range(pos + 1):
-        s += ((-1)**it) * pascal(n,it) * pascal(roll + n - it*N , roll - it*N)
-    return s/N**n
+        s += ((-1)**it)*simplexVolume(numberOfConstraints-it,it+1)*simplexVolume(dim,n - it*constraint)
+    return s
 
-def dice_roll_probability_at_most_simplical(n: int, N: int, roll:int) -> float:
-    roll = roll - n
-    pos = roll // N
-    s = 0
-    for it in range(pos + 1):
-        s += ((-1)**it)*simplical_number(n-it,it+1)*simplical_number(n,roll +1 - it*N)
-    return s/N**n
 
+def diceRollProbability(dice, roll, distribution = "exact roll"):
+    """
+    diceRollProbability(dice, roll, distribution = "exact roll")
+
+    calculates the probability of a particular roll from a set of dice
+
+    dice should be an iterable of integers, roll should be an iterable
+
+    multiple distribution types are supported, namely the probablility of
+    the "exact roll", "at most", "at least", "less than", or "more than"
+
+    e.g. diceRollProbability([6,6,6], 17, distribution = "exact roll") will
+    calculate the probability of getting a 17 from rolling 3 6-sided dice 
+    numbered 1 to 6 (3 in 216)
+
+    diceRollProbability([6,6,6], 17, distribution = "more than") will 
+    calculate the probability of getting more than 17 from rolling 3 6-sided 
+    dice numbered 1 to 6 (1 in 216) 
+    """
+    totalVolume = reduce(op.mul, dice)
+    minimumRoll = numberOfDice = len(dice)
+    maximumRoll = sum(dice)
+    # adjusts the size and dimension of the required simplex based on the distribution;
+    # the way the underlying functions are constructed, it's easiest to think of the values
+    # as being in terms of the "at most" distribution; the "exact roll" distribution has the
+    # same roll but lower dimension than "at most"; "less than" shifts the roll down by 1, etc
+    match distribution:
+        case "exact roll":
+            dim = len(dice)-1
+        case "at most":
+            dim = len(dice)
+        case "less than":
+            dim = len(dice)
+            roll = roll - 1
+        case "more than":
+            dim = len(dice)
+            roll = maximumRoll + minimumRoll - roll - 1
+        case "at least":
+            dim = len(dice)
+            roll = maximumRoll + minimumRoll - roll
+        case _:
+            raise ValueError('distribrution must be either "exact roll", "at most", "less than","more than", or "at least"')
+
+    # checks if the dice are all the same size; this case is optimized to be much faster
+    allSame = True
+    for it in range(1,len(dice)):
+        if dice[it] != dice[it-1]:
+            allSame = False
+            break
+    
+    if allSame:
+        return symmetricalTruncatedSimplexVolume(numberOfDice, dice[0], roll - numberOfDice + 1, dim)/totalVolume
+    else:
+        return truncatedSimplexVolume(dice, roll - numberOfDice + 1, dim)/totalVolume
+
+size = 100
+dice_list = tuple(range(1,size+1))
 
 if __name__ == "__main__":
-    print('\n')
-    simplical_time = timeit.timeit(
-        stmt='[dice_roll_probability_at_most_simplical(100,100,it) for it in range(100,100*100+1)]',
+
+    time = timeit.timeit(
+        stmt='[diceRollProbability(dice_list,it) for it in range(size, size*(size+1)//2)]',
         number = 1,
-        setup = "from __main__ import dice_roll_probability_at_most_simplical"
+        setup = "from __main__ import diceRollProbability, dice_list, size"
+    )
+    # timing a single iteration is generally bad practice, but with a cached function it's necessary:
+    # the later iterations have the benefit of the cache so it doesn't reflect how long it takes
+    # fo run the code from "cold"
+
+    print("using simplical numbers for all probabilities rolling 1d1 + 1d2 + 1d3+...+1d100:",
+        f'{time*1000:.3f}',
+        'ms'
     )
 
-    pascal_time = timeit.timeit(
-        stmt='[dice_roll_probability_at_most_pascal(100,100,it) for it in range(100,100*100+1)]',
+    time = timeit.timeit(
+        stmt='[diceRollProbability([size]*size,it) for it in range(size, size*size+1)]',
         number = 1,
-        setup = "from __main__ import dice_roll_probability_at_most_pascal"
+        setup = "from __main__ import diceRollProbability, dice_list, size"
     )
 
     print("using simplical numbers for all probabilities rolling 100d100:",
-        f'{simplical_time*1000:.3f}',
+        f'{time*1000:.3f}',
         'ms'
     )
 
-    print("using pascal for all probabilities rolling 100d100:",
-        f'{pascal_time*1000:.3f}',
-        'ms'
-    )
 
-    print('\n')
+    values_atMost = [diceRollProbability((2,4,6,7,9),it, "at most") for it in range(5,30 + 1)]
+    values_Exact = [diceRollProbability((2,4,6,7,9),it, "exact roll") for it in range(5,30 + 1)]
+    values_lessThan = [diceRollProbability((2,4,6,7,9),it, "less than") for it in range(5,30 + 1)]
+    values_moreThan = [diceRollProbability((2,4,6,7,9),it, "more than") for it in range(5,30 + 1)]
+    values_atLeast = [diceRollProbability((2,4,6,7,9),it, "at least") for it in range(5,30 + 1)]
+    # values taken from the "normal" (exact roll), "at most", and "at least", distributions for 1d2 + 1d4 + 1d6 + 1d7 + 1d9 on anydice
 
-    pascal_values = [dice_roll_probability_at_most_pascal(5,6,it) for it in range(5,30 + 1)]
-    simplical_values =  [dice_roll_probability_at_most_simplical(5,6,it) for it in range(5,30 + 1)]
-    # values taken from the "at most" distribution for 5d6 on anydice
     anydice_values = [
-        0.0128600823045,
-        0.07716049382710001,
-        0.2700617283951,
-        0.7201646090531,
-        1.6203703703701,
-        3.2407407407401,
-        5.8770576131701,
-        9.7993827160501,
-        15.2006172839501,
-        22.1450617283901,
-        30.5169753086401,
-        39.9691358024701,
-        49.9999999999701,
-        60.0308641974701,
-        69.4830246913001,
-        77.8549382715501,
-        84.79938271599009,
-        90.2006172838901,
-        94.12294238677009,
-        96.75925925920009,
-        98.37962962957009,
-        99.27983539088709,
-        99.72993827154508,
-        99.92283950611308,
-        99.98713991763567,
-        99.99999999994017]
-    print('roll\t\tpascal\t\tsimplex\t\tanydice')
-    for it in range(5,31):
-        print(f'{it}\t\t{pascal_values[it-5]:.5f}\t\t{simplical_values[it-5]:.5f}\t\t{anydice_values[it-5]/100:.5f}')
-
-
-
+        # exact         at least        at most
+        [0,             100,            0],
+        [0.033068783,	100,	        0.033068783],
+        [0.165343915,	99.96693122,	0.198412698],
+        [0.462962963,	99.8015873,	    0.661375661],
+        [0.992063492,	99.33862434,	1.653439153],
+        [1.785714286,	98.34656085,	3.439153439],
+        [2.843915344,	96.56084656,	6.283068783],
+        [4.133597884,	93.71693122,	10.41666667],
+        [5.555555556,	89.58333333,	15.97222222],
+        [6.977513228,	84.02777778,	22.94973545],
+        [8.234126984,	77.05026455,	31.18386243],
+        [9.16005291,	68.81613757,	40.34391534],
+        [9.656084656,	59.65608466,	50],
+        [9.656084656,	50,	            59.65608466],
+        [9.16005291,	40.34391534,	68.81613757],
+        [8.234126984,	31.18386243,	77.05026455],
+        [6.977513228,	22.94973545,	84.02777778],
+        [5.555555556,	15.97222222,	89.58333333],
+        [4.133597884,	10.41666667,	93.71693122],
+        [2.843915344,	6.283068783,	96.56084656],
+        [1.785714286,	3.439153439,	98.34656085],
+        [0.992063492,	1.653439153,	99.33862434],
+        [0.462962963,	0.661375661,	99.8015873],
+        [0.165343915,	0.198412698,	99.96693122],
+        [0.033068783,	0.033068783,	100],
+        [0,             0,              100]
+        ]
+    print('\nLooking at every possibility for 1d2 + 1d4 + 1d6 + 1d7 + 1d9  for multiple distributions:\n')
+    print('\t(EXACT ROLL)\t\t\t(AT LEAST)\t\t\t(AT MOST)\t\t\t(MORE THAN)\t\t\t(LESS THAN)')
+    print('roll\tsimplex\t\tanydice','\tsimplex\t\tanydice','\tsimplex\t\tanydice','\tsimplex\t\tanydice','\tsimplex\t\tanydice' )
+    for it in range(5,29):
+        print(f"{it}\t",
+        f"{values_Exact[it-5]:.5f}\t",f"{anydice_values[it-4][0]/100:.5f}\t",       # exact columns
+        f"{values_atLeast[it-5]:.5f}\t",f"{anydice_values[it-4][1]/100:.5f}\t",     # at least columns
+        f"{values_atMost[it-5]:.5f}\t",f"{anydice_values[it-4][2]/100:.5f}\t",      # at most columns
+        f"{values_moreThan[it-5]:.5f}\t",f"{anydice_values[it-3][1]/100:.5f}\t",    # more than columns
+        f"{values_lessThan[it-5]:.5f}\t",f"{anydice_values[it-5][2]/100:.5f}\t")    # less than columns
---
+++ explanation.md
@@ -0,0 +1,117 @@
+Fair warning, this is mostly just an excuse for me to play with OpenSCAD, a lovely little programming based CAD software.
+
+The first thing to do is to imagine our dice have infinite sides, and each side is represented by a integer along some axis in n-space. We'll work with 3d spaces (so rolling 3 dice) for examples. 
+
+We can imagine the lowest possible roll (1,1,1) as being contained by a unit cube:
+
+![image-20220630185208306](.\images\image1.png)
+
+For the next lowest roll, we can increment any of the dice in the previous lowest roll by 1, giving us (2,1,1), (1,2,1), and (1,1,2). We plop unit cubes of those rolls down at those coordinates. 
+
+![image-20220630185346405](.\images\image2.png)
+
+For the next lowest roll, we do the same thing: we take the previous set of lowest rolls and increment any particular die by one. We will have some repetition this time around; we're only interested in the unique rolls. We get (3,1,1), (2,2,1), (2,1,2), (1,3,1),(1,2,2) and (1,1,3)
+
+![image-20220630190400640](.\images\image3.png)
+
+Intuitively, it's easy to see the volume containing these rolls is an n-simplical number, in this case a 3-simplical number (where a 2-simplical number would be a triangular number). We can calculate the volume containing a particular roll by calculating the n-simplical number associated with it. There are a number of ways to calculate n-simplical numbers, we'll use this definition in python:
+$$
+S(n,x) = \prod_{i=0}^{n-1}\frac{(x + i)}{i + 1}
+$$
+For our purposes, we define negative inputs to return 0. We also have to make an adjustment for the number of dice: for n dice and a given roll, we calculated S(n, roll + 1 - n).D
+
+So we can find the volume of the simplex containing a particular roll (and all the rolls smaller than it), but the obvious problem is that rolls coming from a true set of 3 dice are constrained to a particular rectangular prism. Rolling 3d6, we can roll an 9, but the simplex for a score of 9 pokes outside the true volume of our possibility space:
+
+![image-20220630192844847](.\images\image4.png)
+
+This only gets worse as we look at larger and larger rolls:
+
+![image-20220630192958674](.\images\image5.png)
+
+![image-20220630193025867](.\images\image6.png)
+
+But these protrusions are themselves simplexes! So we can just subtract their volumes off. However, this gets hairy. As we continue to look at larger rolls, the protrusions will eventually overlap. Looking at a roll of 15:
+
+![image-20220630194023451](.\images\image7.png)
+
+I'm running out of good high contrast colours. The protrusions in each direction begin to overlap, and so simply subtracting the protrusions off the total volume no longer works. We could add those overlaps back, which is essentially what I did in my previous attempt at this problem. However, this gets ugly fast if the dice aren't all the same; we have a better way in general. 
+
+We think of the dice as constraining the rolls to particular sections of the axes. We can work with the constraints one at a time. So first we generate the full simplex for a particular roll (in this case rolling a 16 from 1d5 + 1d6 + 1d7):
+
+![image-20220704114446996](.\images\image8.png)
+
+The trick is to recognize that the bits we want to cut off are themselves successively less constrained simplexes. We start by projecting the top face upward, and removing everything that gets caught in that projection. Since we are projecting in the Z direction, we no longer have to worry about constraints in that direction, only the remaining constraints in the X and Y directions:
+
+![image-20220704114723431](.\images\image9.png)
+
+So remove that volume and proceed to another face, in this case the outer face in the Y direction. We again don't need to worry about constraints in the Y direction, but additionally we don't need to worry about constraints in the Z direction either: we took care of the volume directly on top of the prism, so there's no overlap to worry about. We do still need to worry about constraints in the X direction, however. 
+
+![image-20220704115203149](.\images\image10.png)
+
+Now all the remains is the protrusion in the X direction, however with this one we don't need to worry about constraints at all, we just need to calculate its volume:
+
+![image-20220704115415294](C:\Users\Jacob\Downloads\dice\images\image11.png)
+
+Until we're finally left with the portion of the simplex purely contained within the dimensions of our dice.
+
+![image-20220704115525978](.\images\image12.png)
+
+At which point we can find the probability of all rolls up to that point by dividing the volume of the truncated simplex by the volume of the prism. 
+
+So here's how we (or at least I) design python code for this. Obviously we need a way to create unconstrainted simplex volumes. There's a straightforward relationship for that:
+
+``` python
+def simplexVolume(dim,n):
+    """
+    Calculates the volume of the nth simplical number of dimension dim 
+    """
+    if dim < 1 or n < 1:
+        return 0
+    p = 1
+    for it in range(dim):
+        p *= (n + it)/(it + 1)
+    return p
+```
+
+You can also use numpy.nchoosek, or math.comb, (you'll have to adjust the inputs) but for whatever reason I found this faster. 
+
+Then we need a function to calculate a truncated simplex volume. It should obviously be recursive. I designed this function to do that:
+
+```python
+def truncatedSimplexVolume(constraints, n, dim = None):
+    if dim is None:
+        dim = len(constraints)
+    volume = simplexVolume(dim, n)
+    for it in range(len(constraints)):
+        volume -= truncatedSimplexVolume(constraints[it+1:], n-constraints[it], dim)
+    return volume
+```
+
+Setting the dimension as equal to the length of the constraints (i.e. the number of the dice) if an argument isn't provided is just a convenience thing, but otherwise we want to be able to separate number of dice from the dimension: at each stage, we'll be calculating the volume of a simplex with one few constraint, but of the same dimension, so the dimension needs to be a modifiable argument.
+
+We start by calculating the full volume of the simplex for a given roll (remember, n will be the roll - # of dice + 1) and then we iterate through the constraints subtracting off the volume of the protrusion at each stage with one fewer constraint than last time, remembering to adjust n for the protrusion (so if the constraint is 6 and we're looking at a roll of 9, the protrusion sticks out of the volume by 3).
+
+For implementation, you're going to want to be able to cache the values of this function, and using the lru_cache tool means the inputs need to be hashable, i.e. immutable. It's important then that you give constraints in the form of a tuple. It's probably better to create a wrapper function that takes any iterable and translates it to a tuple before calling the above function just for ease of use. 
+
+#### Some further comments
+
+* If you look at the layer we're adding to the simplex every time we go up a roll, they're also simplical numbers, just one dimension down. Those layer volumes also correspond to the probability of getting that particular roll. In fact we can use the exact same approach above, remembering to set dim = len(constraints) - 1, to find the probability of a particular roll instead of the probability of all rolls up to some value; this is the other reason dim is a modifiable argument. I'm too lazy to make another diagram but in the 3D case you can imagine an angled triangular surface of cubes spreading out from the origin, taking up more and then less volume as it passes through and out of the prism. 
+
+* We know these probabilities are symmetrical around the mean; it's probably better in general to only calculate values below/at the mean and adjust as needed. This effectively halves the size of the cache you need. 
+
+* If you're looking at a case where all dice are the same, as was originally asked about, the above approach is unnecessarily slow: too many function calls, each with it's own hash to access the cache. There's a faster way I've detailed here. 
+
+* If you want the entire roll distribution, you probably want to change up the above approach a bit. There's another equation for simplical numbers we can use:
+  $$
+  \begin{align}
+  S(n,x) &= \sum_{i = 1}^{x} S(n-1, i)\\
+  S(0,x) &= 1
+  \end{align}
+  $$
+  This is, effectively, successive cumulative sums. This would be overall slower if you wanted just one value, but if you're doing the entire distribution then finding the cumulative some for the highest roll involves finding the cumulative sum for every roll. I suspect it would be much faster to do it this way, keeping the results of each cumulative sum iteration in the cache for access later. 
+
+* All of this is slower than convolution, unless you're writing in C or Rust or Fortran and can really optimize. Especially if you're looking at the whole distribution, just use convolution. 
+
+* The above approach does work perfectly for impossible rolls, i.e. rolling 3d6, setting the roll value to 2 will give you a probability of 0 (there are no rolls up to 2); setting a roll value of 20 gives a probability of 1: all possible rolls are contained between 0 and 20. The same is true for finding the probability of a particular roll: giving 2 and 20 as the roll both return 0.  
+
+* Using the above approach, I can calculate the values for all possible rolls on a 100d100 in just over 23 seconds; I can calculate the value for any particular roll in about 350 milliseconds (thank goodness for caching). 
